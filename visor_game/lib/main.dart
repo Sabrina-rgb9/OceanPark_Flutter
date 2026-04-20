cd3cb914f83643c23f3e3a135a4a8cb5bd292ea9
Sabrina-rgb9 <selmeftah3.cf@iesesteveterradas.cat>
2026-04-20 16:30:24 +0200

    codigo correguido con imagenes correctas y tamaños

--- visor_game/lib/main.dart
+++ visor_game/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
-// ¡Nueva línea! Importamos los componentes de Flame para poder usar imágenes
-import 'package:flame/components.dart'; 
+import 'package:flame/components.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,19 +14,32 @@ class VisorOceanPark extends FlameGame {
   Future<void> onLoad() async {
     super.onLoad();
     
-    // 1. Cargamos la imagen desde la carpeta assets/images/
-    // (Asegúrate de que el nombre coincida con tu archivo real)
+    // 1. Cargamos el fondo (suponiendo que le quitaste el "size: size" para que mantenga su tamaño real)
     final spriteFondo = await loadSprite('backgrounds/background.png');
-
-    // 2. Creamos el "Componente" que mostrará esa imagen
     final fondo = SpriteComponent(
       sprite: spriteFondo,
-      size: size, // Le decimos que el tamaño sea igual a "size" (el tamaño total de la pantalla)
     );
-
-    // 3. Lo añadimos al lienzo
     add(fondo);
 
-    print("¡Fondo cargado correctamente!");
+    // 2. Cargamos la plataforma
+    final spritePlataforma = await loadSprite('platforms/platform.png');
+    
+    final plataforma = SpriteComponent(
+      sprite: spritePlataforma,
+      
+      // EL CAMBIO ESTÁ AQUÍ: 
+      // Le decimos que de ancho mida lo mismo que la imagen del fondo (fondo.size.x)
+      size: Vector2(fondo.size.x, spritePlataforma.srcSize.y),
+      
+      anchor: Anchor.bottomLeft,
+      
+      // Y LA POSICIÓN TAMBIÉN:
+      // La colocamos en la parte más baja del FONDO, no de la pantalla
+      position: Vector2(0, fondo.size.y),
+    );
+
+    add(plataforma);
+
+    print("¡Fondo y Plataforma sincronizados!");
   }
 }
